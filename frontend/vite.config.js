a9d0a03f73696fe88e32548058c8ae1562d5eb05
Jimmy10ten <s202020393@studentmail.unimap.edu.my>
2026-03-07 01:00:52 +0800

    Fix: Add base path for GitHub Pages

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // Add this line to match your repository name exactly
+  base: "/indonesia-batik-image-classification/",
 })
